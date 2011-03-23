0d05435e3993d5805139edbed2ded75984e4153b
World Wide Web Server <_www@jalter-MBP.local>
2011-03-22 23:56:35 -0700

    added example4.js

---
+++ example4.js
@@ -0,0 +1,33 @@
+// This is my Description
+
+// #Array #Array #Array #Array 
+
+class Git {
+
+	/**
+	 * Create a new git repository
+	 *
+	 * Accepts a creation path, and, optionally, a source path
+	 *
+	 * @access  public
+	 * @param   string  repository path
+	 * @param   string  directory to source
+	 * @return  GitRepo
+	 */	
+	public static function &create($repo_path, $source = null) {
+		return GitRepo::create_new($repo_path, $source);
+	}
+
+	/**
+	 * Open an existing git repository
+	 *
+	 * Accepts a repository path
+	 *
+	 * @access  public
+	 * @param   string  repository path
+	 * @return  GitRepo
+	 */	
+	public static function open($repo_path) {
+		return new GitRepo($repo_path);
+	}
+}
