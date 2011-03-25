948ff91e02ae94719765640eb5a72c4161539d34
World Wide Web Server <_www@jalter-MBP.local>
2011-03-25 01:12:40 -0700

    added ex5.js

--- Config.php
+++ Config.php
@@ -3,10 +3,10 @@
 
 final class Config
 {
-	// SSH git origin. eg. git@github.com:jonalter/test.git
+	// SSH git origin. eg. git@github.com:appcelerator-services/code-samples.git
 	const source = "git@github.com:jonalter/test.git";
 	
-	// path to repo from current dir eg. /Repo/test
+	// path to repo from current dir eg. /Repo/code-samples
 	const repo = "/Repo/test";
 }
 
--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	
 	define("DIR", dirname(__FILE__));
 	$repo_path = DIR.Config::repo;
+	$source = Config::source;
+	$status_message = "Add a code sample to github"; 
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
@@ -15,17 +17,16 @@
 	<form action="<?php $PHP_SELF; ?>" method="POST">
 		<div class="status">
 			<?php
-				// open local repo
-				$repo = Git::open($repo_path);
-				// pull from origin
-				$repo->run('pull');
 				
-				// if (! Git::is_repo($repo)) {
-				// 	echo('Repo: FALSE');
-				// } else {
-				// 	echo('Repo: TRUE');
-				// }
-				
+				// check if repo exists
+				if( is_dir($repo_path) ){
+					// open local repo
+					$repo = Git::open($repo_path);
+				}else{
+					// clone remote repo
+					$repo = new GitRepo($repo_path, true, false);
+					$repo->clone_remote($source);
+				}
 			
 				if( !empty($_POST['file_title']) ){
 					
@@ -40,13 +41,19 @@
 						$f_title = $f_title.".js";
 					}
 				
+					// pull from origin
+					$repo->run('pull');
 					
+					// If file already exits warn and stop
 					$fileName = $repo_path."/".$f_title;
 					if( file_exists($fileName) ){
-						echo('Pick another name, a file with the name \''.$f_title.'\' already exists.');
+						$status_message = 'Pick another name, a file with the name \''
+								.$f_title.'\' already exists.';
 					}else{
 						
-						$file = fopen($fileName,'w') or die ("can't open file");
+						if( !($file = fopen($fileName,'w')) ){
+							$status_message = "Error creating file: ".$fileName;
+						}
 						
 						// DESCRIPTION
 						// comment description
@@ -62,20 +69,27 @@
 							}
 							$f_tags = $f_tags.$tag." ";
 						}
-						
+						try{
 						fwrite($file,$f_desc."\n\n");
 						fwrite($file,$f_tags."\n\n");
 						fwrite($file,$f_code."\n");
 						fclose($file);
-						
-						// `git add *`
-						$repo->add('*');
-						
-						// `git commit -av -m 'added files'`
-						$commit_message = "added ".$f_title;
-						$repo->commit($commit_message);
-						// push out
-						$repo->run('push origin master');
+						}catch (Exception $e) {
+							$status_message = "Error writing to file: ".$f_title;
+						}
+						try{
+							// `git add *`
+							$repo->add('*');
+							// `git commit -av -m 'added files'`
+							$commit_message = "added ".$f_title;
+							$repo->commit($commit_message);
+							// push out
+							$repo->run('push origin master');
+							
+							$status_message = 'Successfully created file: '.$f_title;
+						}catch (Exception $e) {
+							$status_message = 'Error pushing to GitHub';
+						}
 						
 						// Unset all POST params
 						unset($_POST['file_title']);
@@ -87,17 +101,23 @@
 			?>
 		</div>
         <div class="file">
-            <label for="file_title">Title</label>
-            <input type="text" id="file_title" name="file_title" class="fileTitle" value="<?php echo( $_POST['file_title'] ) ?>" /><br/>
+			<div class="file_status" >
+				<?php echo($status_message) ?>
+			</div>
+            <label for="file_title">Title/File Name:</label>
+            <input type="text" id="file_title" name="file_title" size="70" 
+				class="fileTitle" value="<?php echo( $_POST['file_title'] ) ?>" /><br/>
             <label for="file_description">Description</label>
-            <input type="text" id="file_description" name="file_description" class="fileDescription" value="<?php echo( $_POST['file_description'] ) ?>" /><br/>
+            <input type="text" id="file_description" name="file_description" size="70"
+ 				class="fileDescription"
+				value="<?php echo( $_POST['file_description'] ) ?>" /><br/>
 			<label for="file_tags">Tags</label>
-            <input type="text" id="file_tags" name="file_tags" class="fileTags" value="<?php echo( $_POST['file_tags'] ) ?>" /><br/>
+            <input type="text" id="file_tags" name="file_tags" size="70"
+				class="fileTags" value="<?php echo( $_POST['file_tags'] ) ?>" /><br/>
             <label for="file_code">Code</label>
-            <textarea rows="5" cols="70" id="file_code" name="file_code" ><?php echo( $_POST['file_code'] ) ?></textarea>
+            <textarea rows="5" cols="70" id="file_code" name="file_code" >
+				<?php echo( $_POST['file_code'] ) ?></textarea>
 	    </div>
-	    <input type="hidden" name="login" value="dawsontoth" />
-	    <input type="hidden" name="token" value="**INSERT TOKEN HERE**" />
 	    <input type="submit" id="createFile" value="Add File"/>
 	</form>
 </body>
